4f0992e799fef063e04410e434749155f3d29e6b
Ryan Chui <ryanc@23andme.com>
2018-02-18 22:47:11 -0800

    Testing default trait implementation.

--- hello_world/hello_world/src/main.rs
+++ hello_world/hello_world/src/main.rs
@@ -5,17 +5,21 @@ struct Message {
 }
 
 trait Concatable {
-    fn concat(&self) -> String;
-}
-
-impl Concatable for Message {
     fn concat(&self) -> String {
-        let mut output = String::from("");
-        output.push_str(&self.text);
-        output.push_str(&self.name);
-        output
+        String::from("Nothing to concatenate...")
     }
 }
+
+impl Concatable for Message { }
+
+// impl Concatable for Message {
+//     fn concat(&self) -> String {
+//         let mut output = String::from("");
+//         output.push_str(&self.text);
+//         output.push_str(&self.name);
+//         output
+//     }
+// }
 
 fn main() {
     let message = Message{text: String::from("Hello World "), name: String::from("Ryan")};
